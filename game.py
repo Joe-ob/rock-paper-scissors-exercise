4d550a94b36cd349a96beae63a0a595c50e34f6b
JoeObrien <jpo52@georgetown.edu>
2021-02-04 11:23:17 -0500

    Second Commit
    Computer randomly selects output

--- game.py
+++ game.py
@@ -1,3 +1,47 @@
 # game.py
 
+
+
+
+# introduction
+print("------------------")
+print("Welcome 'player one' to my Rock-Paper-Scissors game...")
+print("-------------------")
+
+# Asking user for an Input
+user_choice=input("Please choose either 'rock', 'paper', or 'scissors': ")
+
+
+
+# Repeating selection
+print(f"You chose: {user_choice}")
+
+
+#computer makes selection
+#options = ['rock,', 'paper', 'scissors']
+
+import random
+computer_choice=["Rock", "Paper", "Scissors"]
+print(f"The computer chose: {random.choice(computer_choice)}")
+
+
+
+exit()
+
+
+
+
+
+
+
+
+
+
+#Naming a Winner
 print("Rock, Paper, Scissors, Shoot!")
+print("-------------------")
+print("Oh, the computer won. It's ok.")
+print("-------------------")
+
+#Goodbye
+print("Thanks for playing. Please play again!")
